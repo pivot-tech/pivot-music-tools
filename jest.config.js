a83acf16f4f5224c3b7f0e0cc63a48e368c71dca
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2020-12-19 09:09:58 -0600

    build(mocking): support ts-auto-mock

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+    globals: {
+        'ts-jest': {
+            compiler: 'ttypescript',
+        },
+    },
     preset: 'ts-jest',
     testEnvironment: 'node',
+    setupFiles: ['<rootDir>setupTests.ts'],
 }
